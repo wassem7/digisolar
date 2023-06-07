56e13ba9855ad4a9713db48e7ef05b2b12bda7c1
wassem7 <waseemdarkwa@gmail.com>
2023-06-07 17:51:45 +0100

    initialization

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/router";
+const SignIn: NextPage = () => {
+  const router = useRouter();
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    const res = await signIn("google", {
+      redirect: false,
+      callbackUrl: `${window.location.origin}/dashboardhome`,
+    });
+
+    // console.log(res);
+  };
+  return (
+    <>
+      <div className="flex min-h-screen flex-col justify-center bg-slate-800 py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 bg-gradient-to-r from-amber-200 to-yellow-400 bg-clip-text text-center text-3xl font-extrabold text-transparent ">
+            WATER PURITY CHECKER
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-slate-800 px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md border border-transparent  bg-yellow-400 px-4 py-2 text-sm font-medium text-white  shadow-sm hover:bg-[#f2b722] focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2"
+                >
+                  Sign in with Google
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SignIn;
