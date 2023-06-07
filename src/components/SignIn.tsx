70734e3222ffab7925213b55c2208e1d5f65718c
wassem7 <waseemdarkwa@gmail.com>
2023-06-07 20:00:54 +0100

    heading change

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -16,9 +16,9 @@ const SignIn: NextPage = () => {
   };
   return (
     <>
-      <div className="flex min-h-screen flex-col justify-center bg-slate-100 py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text text-center text-3xl font-extrabold text-transparent ">
+      <div className="mx-auto flex min-h-screen flex-col justify-center bg-slate-100 py-12 sm:px-6 lg:px-8">
+        <div className="mx-auto   sm:w-full sm:max-w-md">
+          <h2 className="mt-6  bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text text-center text-3xl font-extrabold text-transparent ">
             WATER PURITY CHECKER
           </h2>
         </div>
@@ -29,7 +29,7 @@ const SignIn: NextPage = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md border border-transparent  bg-blue-400 px-4 py-2 text-sm font-medium text-white  shadow-sm hover:bg-[#06cbe5] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+                  className="flex w-full justify-center rounded-md border border-transparent  bg-blue-400 px-4 py-2 text-sm font-medium text-white  shadow-sm hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
                 >
                   Sign in with Google
                 </button>
