76c6a3755aabb0674d4edcad010be2f803dbd8b7
wassem7 <waseemdarkwa@gmail.com>
2023-06-08 15:44:50 +0100

    dashboard stats section

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,87 @@
 import React from "react";
 
-const Homepage = () => {
-  return <div>Homepage, I'll work on it later charle.</div>;
-};
+/* This example requires Tailwind CSS v2.0+ */
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
 
-export default Homepage;
+const stats = [
+  {
+    name: "PH VALUE",
+    stat: "6.5",
+    previousStat: "5.0",
+    change: "12%",
+    changeType: "increase",
+  },
+  {
+    name: "TURBIDITY VALUE",
+    stat: "0.8",
+    previousStat: "0.6",
+    change: "2.02%",
+    changeType: "increase",
+  },
+  {
+    name: "CONDUCTIVITY VALUE",
+    stat: "300",
+    previousStat: "350",
+    change: "4.05%",
+    changeType: "decrease",
+  },
+];
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Homepage() {
+  return (
+    <div>
+      <h3 className="text-lg font-bold leading-6 text-sky-900">
+        Last 24 hours
+      </h3>
+      <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
+        {stats.map((item) => (
+          <div key={item.name} className="px-4 py-5 sm:p-6">
+            <dt className="text-base font-normal text-gray-900">{item.name}</dt>
+            <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
+              <div className="flex items-baseline text-2xl font-semibold text-sky-600">
+                {item.stat}
+                <span className="ml-2 text-sm font-medium text-gray-500">
+                  from {item.previousStat}
+                </span>
+              </div>
+
+              <div
+                className={classNames(
+                  item.changeType === "increase"
+                    ? "bg-green-100 text-green-800"
+                    : "bg-red-100 text-red-800",
+                  "inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
+                )}
+              >
+                {item.changeType === "increase" ? (
+                  <ArrowUpIcon
+                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <ArrowDownIcon
+                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                    aria-hidden="true"
+                  />
+                )}
+
+                <span className="sr-only">
+                  {" "}
+                  {item.changeType === "increase"
+                    ? "Increased"
+                    : "Decreased"}{" "}
+                  by{" "}
+                </span>
+                {item.change}
+              </div>
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  );
+}
--- src/pages/dashboardhome.tsx
+++ src/pages/dashboardhome.tsx
@@ -95,7 +95,7 @@ export default function Dashboard() {
                   <div className="mt-5 h-0 flex-1 overflow-y-auto">
                     <nav className="space-y-1 px-2">
                       {navigation.map((item) => (
-                        <a
+                        <Link
                           key={item.name}
                           href={item.href}
                           className={classNames(
@@ -115,7 +115,7 @@ export default function Dashboard() {
                             aria-hidden="true"
                           />
                           {item.name}
-                        </a>
+                        </Link>
                       ))}
                     </nav>
                   </div>
@@ -133,12 +133,14 @@ export default function Dashboard() {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5">
             <div className="flex flex-shrink-0 items-center px-4">
-              {/* <img
+              <Image
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              /> */}
-              <h1 className="bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text text-center  font-extrabold text-transparent">
+                src="/waterlogo.png"
+                width={1080}
+                height={1080}
+                alt="Water Purity Checker"
+              />
+              <h1 className="bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text text-center text-sm  font-extrabold text-transparent">
                 WATER PURITY CHECKER
               </h1>
             </div>
@@ -182,7 +184,11 @@ export default function Dashboard() {
               <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
             </button>
             <div className="flex flex-1 justify-between px-4">
-              <div className="flex flex-1"></div>
+              <div className="flex flex-1 items-center">
+                {/* <h1 className="bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text text-center  font-extrabold text-transparent">
+                  WATER PURITY STATS
+                </h1> */}
+              </div>
               <div className="ml-4 flex items-center justify-end  md:ml-6">
                 {/* <button
                   type="button"
@@ -251,9 +257,9 @@ export default function Dashboard() {
           <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">
+                {/* <h1 className="text-2xl font-semibold text-sky-500">
                   Dashboard
-                </h1>
+                </h1> */}
               </div>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <Homepage />
