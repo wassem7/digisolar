1513d466aacda3c300ab0cce2f7bd47ac2bc6afb
wassem7 <waseemdarkwa@gmail.com>
2023-07-01 12:21:19 +0100

    POST REQUEST CONFIG

--- src/server/api/routers/reading.ts
+++ src/server/api/routers/reading.ts
@@ -9,11 +9,52 @@ import {
 } from "@/server/api/trpc";
 
 export const readingRouter = createTRPCRouter({
-  create: publicProcedure.mutation(async ({ ctx, input }) => {
-    console.log(input);
+  createph: publicProcedure
+    // .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      console.log("POST REQUEST");
 
-    // return ctx.prisma.readings.create({
-    //   data: { input },
-    // });
+      console.log(input);
+
+      // return ctx.prisma.ph.create({
+      //   data: { ph: input.ph },
+      // });
+      return {
+        status: "Data received !",
+        message: input,
+      };
+    }),
+
+  all: publicProcedure.query(async ({ ctx }) => {
+    console.log("yo");
+
+    return {
+      data: "Hello this is a get request !",
+    };
   }),
+
+  createPh: publicProcedure
+    .meta({
+      openapi: { method: "POST", path: "/create/{ph}", enabled: true },
+    })
+    .input(z.object({ ph: z.string() }))
+    .output(z.object({ ph: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      console.log(input.ph);
+
+      return await ctx.prisma.ph.create({
+        data: { ph: input.ph },
+      });
+      // return { ph: ` ${input.name}` };
+    }),
+
+  getHello: publicProcedure
+    .meta({
+      openapi: { method: "GET", path: "/say-hello", enabled: true },
+    })
+    .input(z.object({ name: z.string() }))
+    .output(z.object({ greeting: z.string() }))
+    .query(({ input }) => {
+      return { greeting: ` ${input.name}` };
+    }),
 });
