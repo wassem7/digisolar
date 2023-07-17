9a7ff58e048af49c856ff3f3a81a52da27c44771
wassem7 <waseemdarkwa@gmail.com>
2023-07-17 10:23:32 +0100

    remove web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { Roboto_Mono } from "@next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+
 const robotomono = Roboto_Mono({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
@@ -17,7 +17,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <main className={robotomono.className}>
         <Component {...pageProps} />
-        <Analytics />
       </main>
     </SessionProvider>
   );
