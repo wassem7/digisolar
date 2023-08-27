b67bab2785c0b013d733cebff8dcda7a72475d6e
wassem7 <waseemdarkwa@gmail.com>
2023-08-27 12:28:38 +0000

    removed auth for preview

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -12,19 +12,19 @@ export default function Homepage() {
   const { data: sessionData } = useSession();
   const router = useRouter();
   console.log("SESSION DATA", sessionData);
-  if (!sessionData) {
-    return (
-      <div className="flex min-h-screen flex-col items-center justify-center bg-black">
-        <h1 className="text-white">Login to continue</h1>
-        <button
-          className="mt-2 rounded-lg bg-yellow-400 px-3 py-2 hover:cursor-pointer"
-          onClick={() => router.push("/login")}
-        >
-          SignIn
-        </button>
-      </div>
-    );
-  }
+  // if (!sessionData) {
+  //   return (
+  //     <div className="flex min-h-screen flex-col items-center justify-center bg-black">
+  //       <h1 className="text-white">Login to continue</h1>
+  //       <button
+  //         className="mt-2 rounded-lg bg-yellow-400 px-3 py-2 hover:cursor-pointer"
+  //         onClick={() => router.push("/login")}
+  //       >
+  //         SignIn
+  //       </button>
+  //     </div>
+  //   );
+  // }
   return (
     <Layout>
       <Weather />
